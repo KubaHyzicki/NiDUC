88f3c98f79e29da61bfe9a1a48c1e14ea89fe4cf
KubaHyzicki <kuba.hyzicki@gmail.com>
2018-04-16 23:11:42 +0200

    Usunięty plik z metodą o starej nazwie

--- canal.m
+++
@@ -1,8 +0,0 @@
-function [Signal] = canal (Signal,sigma,amplify)
-#tu teoretycznie można dodać jeszcze jakieś niwelowanie zakłóceń w postaci np. wzmocnienia sygnału
-for i=1:(length(Signal))
-  Signal(i)*=amplify;
-  Signal(i)=Signal(i)+normrnd(0,sigma);
-  Signal(i)/=amplify;
-end
-endfunction
