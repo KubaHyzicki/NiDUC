e7b221caa6059afc597b6b58c476b25fcae3a145
KubaHyzicki <kuba.hyzicki@gmail.com>
2018-06-03 19:29:33 +0200

    final? II commit

--- main.m
+++ main.m
@@ -43,20 +43,7 @@ BERvB=[];
 EvB=[];
 BERvN=[];
 EvN=[];
-%for i=1:(length(missedBitsVB))
-%  BERvB(i)=missedBitsVB(i)/(recivedBitsVB(i));
-%  if missedBitsVB(i)>0                              %możliwie najmniej wpływające na wynik rozwiązanie problemu ewentualnego dzielenia przez zero
-%    EvB(i)=correctBitsVB(i)/(missedBitsVB(i));
-%  else
-%    EvB(i)=correctBitsVB(i)/(missedBitsVB(i)+intmin);
-%  end
-%  BERvN(i)=missedBitsVN(i)/(recivedBitsVN(i));
-%  if missedBitsVB(i)>0
-%    EvN(i)=correctBitsVN(i)/(missedBitsVN(i));
-%  else
-%    EvN(i)=correctBitsVN(i)/(missedBitsVN(i)+intmin);
-%  end
-%end
+
 for i=1:(length(missedBitsVB))
   BERvB(i)=missedBitsVB(i)/(recivedBitsVB(i));
   EvB(i)=correctBitsVB(i)/(recivedBitsVB(i));
