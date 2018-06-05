7271a2429be1014ecc4ea4165c3ad5d95d1eb94f
KubaHyzicki <kuba.hyzicki@gmail.com>
2018-06-05 20:17:37 +0200

    Symulacja pojedynczego punktu w testMain

--- testMain.m
+++ testMain.m
@@ -3,17 +3,7 @@ close all;
 clear all;
 
 %ustawianie zmiennych-poniżej są podane przykładowe wartości, dla których widać już efekty kanałów
-n=100;       %długość sygnału
-frameSize=10; %długość pojedynczej ramki
-range=0.005;    %szansa na przekłamanie bitu(od 0 do 1)
-sigma=4;      %wpływa na rozrzut wartości szumu-taka duża wartość, bo test jest przy 5-krotnym wzmocnieniem sygnału
-amplify=5;    %wzmocnienie sygnału
-clc;
-close all;
-clear all;
-
-%ustawianie zmiennych-poniżej są podane przykładowe wartości, dla których widać już efekty kanałów
-n=10;        %długość sygnału
+n=10000;        %długość sygnału
 frameSize=10; %długość pojedynczej ramki
 range=0.005;  %szansa na przekłamanie bitu(od 0 do 1)
 sigma=4;      %wpływa na rozrzut wartości szumu-taka duża wartość, bo test jest przy 5-krotnym wzmocnieniem sygnału
@@ -28,21 +18,46 @@ elseif (m == 8)
 else
   "ciastko"
 end
-D
 
+recivedBits=0;
+missedBits=0;
+correctBits=0;
 
 signal=[];
 signal=generateRand(n);
-signal
-coded=codeCRC(signal,D);
-coded
-corr=checkCRC(coded,D)
-coded(4)=~coded(4);
-coded
-corr=checkCRC(coded,D)
-%tempVec=canalVB(tempVec,range);
-%tempVec=decoder(tempVec);
-%uncoded=checkCRC(signal,frameSize,16);
-%uncoded
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tempVec=[];
+frameStart=1;
+while(true)
+%ustalanie granic ramki
+  tempFrameSize=frameSize;
+  frameEnd=frameStart+frameSize+m-1;
+  if frameEnd>n
+    frameEnd=n;
+    tempFrameSize=frameEnd-frameEnd+1;
+  end
+%przepuszczanie sygnału przez symulację
 
-#signalSimulation(n,frameSize,sigma,range,amplify)
+%  tempVec=codeParityBit(signal(frameStart:frameEnd),tempFrameSize);
+  tempVec=codeCRC(signal(frameStart:frameEnd),D);
+  
+  tempVec=canalVB(tempVec,range);
+  tempVec=decoder(tempVec);
+  
+  recivedBits+=tempFrameSize;
+%  if checkBitParity(tempVec,tempFrameSize)(1)==1
+  if checkCRC(tempVec,D)==1
+    frameStart+=tempFrameSize;                      %poprawnie przesłana paczka
+    if frameEnd==n
+      break;
+    end
+  else
+    missedBits+=tempFrameSize;                    %niepoprawnie przesłana paczka-powtórzenie pętli
+  end
+end
+
+correctBits=recivedBits-missedBits;
+BER=missedBits/(recivedBits);
+E=correctBits/(recivedBits);
+E
+BER
