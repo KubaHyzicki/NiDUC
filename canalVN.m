f1de36f9d2a793d8d9618485d88b94047800efcd
KubaHyzicki <kuba.hyzicki@gmail.com>
2018-06-12 18:37:33 +0200

    Add files via upload

--- canalVN.m
+++ canalVN.m
@@ -1,7 +1,9 @@
 function [Signal] = canalVN (Signal,sigma,amplify)
-for i=1:(length(Signal))
-  Signal(i)*=amplify;
-  Signal(i)=Signal(i)+normrnd(0,sigma);
-  Signal(i)=Signal(i)/amplify;
-end
+  for i=1:(length(Signal))
+    Signal(i)*=amplify;
+%rozciągnięcie również zera(dla niego samo wymnożenie nie działa)
+    if Signal(i)==0
+      Signal(i)=1-amplify;
+    Signal(i)=Signal(i)+normrnd(0,sigma);
+  end
 endfunction
