b2b70d4284b9163a0cf48e8a7d96f6120005295b
KubaHyzicki <kuba.hyzicki@gmail.com>
2018-06-12 17:16:02 +0200

    Add files via upload

--- canalVN.m
+++ canalVN.m
@@ -1,8 +1,7 @@
 function [Signal] = canalVN (Signal,sigma,amplify)
-#tu teoretycznie można dodać jeszcze jakieś niwelowanie zakłóceń w postaci np. wzmocnienia sygnału
 for i=1:(length(Signal))
   Signal(i)*=amplify;
   Signal(i)=Signal(i)+normrnd(0,sigma);
-  Signal(i)/=amplify;
+  Signal(i)=Signal(i)/amplify;
 end
 endfunction
